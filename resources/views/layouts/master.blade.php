8230b30362494debde872b5235afe66d72047f89
Fabian Jevon <Shar_evonz@hotmail.com>
2024-12-17 06:30:09 +0700

    update front-end

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Keyword Density Tool</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -43,7 +43,7 @@
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 @yield('scripts')
 </body>
 </html>
--- resources/views/tool/index.blade.php
+++ resources/views/tool/index.blade.php
@@ -1,22 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="html-text-tab" data-bs-toggle="tab" data-bs-target="#html-text-tab-pane" 
+        type="button" role="tab" aria-controls="html-text-tab-pane" aria-selected="true">HTML or Text</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="url-tab" data-bs-toggle="tab" data-bs-target="#url-tab-pane" 
+        type="button" role="tab" aria-controls="url-tab-pane" aria-selected="false">URL</button>
+    </li>
+</ul>
 <form id="keywordDensityInputForm" method="POST">
-    <div class="form-group">
-        <label for="keywordDensityInput">HTML or Text</label>
-        <textarea class="form-control" id="keywordDensityInput" rows="12"></textarea>
+    <div class="form-group ">
+        <div class="tab-content py-3 " id="myTabContent" style="min-height: 350px;">
+            <div class="tab-pane fade show active" id="html-text-tab-pane" role="tabpanel" aria-labelledby="html-text-tab" tabindex="0">
+                <textarea class="form-control" id="keywordDensityTextArea" rows="12"></textarea>
+            </div>
+            <div class="tab-pane fade" id="url-tab-pane" role="tabpanel" aria-labelledby="url-tab" tabindex="0">
+                <input class="form-control" type="text" id="keywordDensityURL" name="keywordDensityURL"/>
+            </div>
+        </div>
+        
     </div>
     <button type="submit" class="btn btn-primary mb-2">Get Keyword Densities</button>
 </form>
+<div id="keywordDensityResult">
+
+</div>
+
 @endsection
 
 @section ('scripts')
     <script>
-        $('#keywordDensityInputForm').on('submit', function (e) { // Listen for submit button click and form submission.
-            e.preventDefault(); // Prevent the form from submitting
-            let kdInput = $('#keywordDensityInput').val(); // Get the input
-            if (kdInput !== "") { // If input is not empty.
-            // Set CSRF token up with ajax.
+        $(".nav-link").click(function(){
+            $("#keywordDensityTextArea, #keywordDensityURL").val("");
+        });
+        $('#keywordDensityInputForm').on('submit', function (e) {
+            e.preventDefault();
+            let textInput = $('#keywordDensityTextArea').val();
+            let urlInput = $('#keywordDensityURL').val();
+
+            if (textInput !== "" || urlInput !== "") {
+                // Set CSRF token up with ajax.
                 $.ajaxSetup({
                     headers: {
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -26,7 +52,7 @@
                 $.ajax({ // Pass data to backend
                     type: "POST",
                     url: "/tool/calculate-and-get-density",
-                    data: {'keywordInput': kdInput},
+                    data: {'textInput': textInput, 'urlInput':urlInput},
                     success: function (response) {
                         // On Success, build a data table with keyword and densities
                         if (response.length > 0) {
@@ -44,7 +70,7 @@
 
                             html += "</tbody></table>";
 
-                            $('#keywordDensityInputForm').after(html); // Append the html table after the form.
+                            $('#keywordDensityResult').html(html); // Append the html table after the form.
                         }
                     },
                 });
